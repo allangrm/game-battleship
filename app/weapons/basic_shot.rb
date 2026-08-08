b22578cf849a5dfc0ff049738b70935b17fae880
Júlio Pedro <juliop.dev@gmail.com>
2026-08-08 11:37:27 -0300

    fix: Sintax error

--- app/weapons/basic_shot.rb
+++ app/weapons/basic_shot.rb
@@ -3,10 +3,10 @@
 # @author Júlio Pedro
 # @version 1.0
 # @since 07-08-2026
-require_relative "weapon"
+require_relative "Weapon"
 
 # Ataque básico, atira em uma única célula. (RF06)
-class Basic_shot < weapon
+class Basic_shot < Weapon
   def target_cells(row, col, board, **_opts)
     valid_cells([[row, col]], board)
   end
